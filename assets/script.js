48a990d73f40dfcb32b7c025c91d0370f69fc965
ЧакНорисс <64031009+Ility-47@users.noreply.github.com>
2022-10-06 19:34:17 +0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+alert('Попади молотком по кроту!')
+
+
+let nora = document.querySelector('.nora');
+
+function krotolovka(){
+    let krot = document.createElement('div');
+    krot.classList.add('krot')
+    nora.appendChild(krot);
+
+    let krotImg = document.createElement('img');
+    krotImg.src = "assets/krot.png";
+    krot.appendChild(krotImg)
+
+    function anim(){
+        let i = 7
+        do {
+            krot.style.top = i *'0';
+            krot.style.top = 14 * i + 'px';
+            i -= 1
+        } while(i > 0);
+
+
+    }
+    setTimeout(anim,2000);
+}
+
+krotolovka();
+
+
+function scrim() {
+    let pig = document.querySelector('.pig');
+    pig.style.display = 'block';
+    let audio = new Audio();
+    audio.src = 'assets/pizda.mp3';
+    audio.autoplay = true;
+    console.log(audio.volume)
+}
