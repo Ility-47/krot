54e297bf09551063b32618566f1a2f15e9d073a5
ЧакНорисс <64031009+Ility-47@users.noreply.github.com>
2022-10-11 21:43:09 +0300

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,57 @@
 alert('Попади молотком по кроту!')
 
-
-let nora = document.querySelector('.nora');
-
-function krotolovka(){
-    let krot = document.createElement('div');
-    krot.classList.add('krot')
-    nora.appendChild(krot);
-
-    let krotImg = document.createElement('img');
-    krotImg.src = "assets/krot.png";
-    krot.appendChild(krotImg)
-
-    function anim(){
-        let i = 7
-        do {
-            krot.style.top = i *'0';
-            krot.style.top = 14 * i + 'px';
-            i -= 1
-        } while(i > 0);
-
-
-    }
-    setTimeout(anim,2000);
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-krotolovka();
+
+
+let krot = document.createElement('div');
+let krotImg = document.createElement('img');
 
 
 function scrim() {
+    let pig = document.querySelector('.pig');
+    pig.style.display = 'block';
     let audio = new Audio();
     audio.src = 'assets/pizda.mp3';
     audio.autoplay = true;
-    console.log(audio.volume);
-    let pig = document.querySelector('.pig');
-    pig.style.display = 'block';
+    console.log(audio.volume)
 }
+
+function krotolovka() {
+    let num = getRandomIntInclusive(1, 7);
+    let nora = document.getElementById(num);
+    console.log(num);
+
+
+    krot.classList.add('krot');
+    nora.appendChild(krot);
+
+    krotImg.setAttribute('id', 'elem');
+    krotImg.src = "assets/krot.png";
+    krot.appendChild(krotImg);
+
+    function vilaz(){
+        elem.addEventListener('click', () => scrim())
+    }
+
+    setTimeout(vilaz, 1000)
+
+    function anim(){
+            krotImg.style.top =  0;
+            krotImg.style.bottom = 340 + 'px';
+            function skip(){
+                krotImg.style.top = '110px';
+            }
+            setTimeout(skip, 3000);
+    }
+
+    setTimeout(anim, 500)
+
+
+}
+
+setInterval(krotolovka, 5000)
